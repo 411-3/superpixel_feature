956025ca30d3134c2369647a40dd7a26401d836a
411-3 <2250611484@qq.com>
2017-02-20 11:51:41 +0800

    superpixel_feature

---
+++ CMakeFiles/superpixel_feature.dir/DependInfo.cmake
@@ -0,0 +1,13 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/xduser/LiHuan/SuperPixel_Feature/j2seg.cpp" "/home/xduser/LiHuan/SuperPixel_Feature/CMakeFiles/superpixel_feature.dir/j2seg.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
---
+++ CMakeFiles/superpixel_feature.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/superpixel_feature.dir/j2seg.o"
+  "superpixel_feature.pdb"
+  "superpixel_feature"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/superpixel_feature.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
